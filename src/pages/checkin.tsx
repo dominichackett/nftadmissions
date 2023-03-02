4795ca8b9bc70e887317a760ebfabea1415487dc
Dominic Leon Hackett <dominichackett@gmail.com>
2023-03-02 11:39:54 -0400

    Update checkin.tsx

--- src/pages/checkin.tsx
+++ src/pages/checkin.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header/header'
 import Footer from '@/components/Footer/footer'
-import QrReader  from 'react-qr-reader';
+import QrReader  from 'react-qr-scanner';
 import { useState,useEffect } from 'react';
 import { XCircleIcon ,CheckCircleIcon,QuestionMarkCircleIcon} from "@heroicons/react/24/solid";
 import { ethers } from 'ethers';
